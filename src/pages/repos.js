f735115747be9eda40f247f980435b13b68bfdc4
Will Buck <william.parker.buck@gmail.com>
2015-06-25 15:16:36 -0500

    WIP thurspm now with repos

--- src/pages/repos.js
+++ src/pages/repos.js
@@ -1,13 +1,23 @@
 /// <reference path="../typings/react/react.d.ts" />
 import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
 
 export default React.createClass({
+	mixins: [ampersandMixin],
+	
 	displayName: 'Repos',
 	
 	render () {
+		const {repos} = this.props
+		
 		return (
 			<div>
 				<h2>Repos</h2>
+				<ul>
+				{repos.map((repo) => 
+					<li><a href="">{repo.full_name}</a></li>
+				)}
+				</ul>
 			</div>
 		)
 	}	
